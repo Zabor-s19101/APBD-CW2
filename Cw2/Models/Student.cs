eae3d146693a925d8b34aca4cb163b238534241b
Zaborejszyn <zabor1274@gmail.com>
2020-03-11 13:22:36 +0100

    Added Student class

---
+++ Cw2/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cw2.Models {
+    [Serializable]
+    public class Student {
+        public string IndexNumber { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Birthdate { get; set; }
+        
+    }
+}
