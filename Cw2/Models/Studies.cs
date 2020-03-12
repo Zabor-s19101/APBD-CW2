abf3cdb447e81b8282c6ba754f41757f75117c0c
Zaborejszyn <zabor1274@gmail.com>
2020-03-12 23:56:23 +0100

    Added classes for serialization

---
+++ Cw2/Models/Studies.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace Cw2.Models {
+    [XmlRoot("studies")]
+    public class Studies {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("mode")]
+        public string Mode { get; set; }
+    }
+}
